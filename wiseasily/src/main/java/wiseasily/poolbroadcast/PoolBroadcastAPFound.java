d4bb81bfc9a48f688b45845232019b2a57b78d6b
putrabangga <mohamad.damba@efishery.com>
2020-03-20 13:39:22 +0700

    fixing handler

--- wiseasily/src/main/java/wiseasily/poolbroadcast/PoolBroadcastAPFound.java
+++ wiseasily/src/main/java/wiseasily/poolbroadcast/PoolBroadcastAPFound.java
@@ -6,11 +6,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.util.Log;
-
 import com.efishery.putrabangga.wiseasily.BuildConfig;
-
 import wiseasily.source.SourceCallback;
 import wiseasily.util.WifiUtil;
 
@@ -29,7 +28,7 @@ public class PoolBroadcastAPFound extends BroadcastReceiver  {
     private final WifiUtil wifiUtil;
     private SourceCallback.APFoundCallback apFoundCallback;
     private final WifiManager mWifiManager;
-    private Handler mHandler;
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
     private int count = 0;
     private final Runnable mOutOfTime = new Runnable() {
         @Override
@@ -55,7 +54,6 @@ public class PoolBroadcastAPFound extends BroadcastReceiver  {
         wifiUtil = new WifiUtil();
         this.mContext = context;
         this.ssid = ssid;
-        mHandler = new Handler();
         mWifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         poolBroadcastWifiOff = new PoolBroadcastWifiOff(mContext);
     }
@@ -63,7 +61,6 @@ public class PoolBroadcastAPFound extends BroadcastReceiver  {
     public PoolBroadcastAPFound(@NonNull Context context, @NonNull String ssid) {
         this.mContext = context;
         this.ssid = ssid;
-        mHandler = new Handler();
         wifiUtil = new WifiUtil();
         mWifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         poolBroadcastWifiOff = new PoolBroadcastWifiOff(mContext);
